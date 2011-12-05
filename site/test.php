c8320f7ac005f87022612c8da2817bc573e4d171
pmoulos@gmail.com <pmoulos@gmail.com@14a212b1-cf97-39a4-918a-3a479dd5a06d>
2011-12-05 17:46:32 +0000

    Added basic PHP/AJAX/jQuery functional templates

---
+++ site/test.php
@@ -0,0 +1,10 @@
+<?php
+	$genes = array('TGFB1','SMAD2','SMAD3','SMAD4','BMP7','CTGF','TGFBR1','SMAD7','AGTR1','ACE');
+	$species = 9606;
+	$datasets = array('doucet_garonne','sadlier_johnson_d5qpcr','sadlier_johnson_d5','song_pei_adpkdkidney','zhao_tilton_mouserenalcortex',
+					  'iphone_mrna','hkupp_rodentimcd_db','hkupp_humanglomerulus_db','sigdel_sarwal_humanurineacuterejection',
+					  'hkupp_humanurinaryexosome_db','rooney_crean_humankidneydiabet','gse11221_detrusor','gse11221_urothelium','gse22464',
+					  'huttlin_mouse_phospho','gse1009','huttlin_mouse_nonphospho','gse17141','gse4816','vontoerne_nelson_ifta_14d',
+					  'vontoerne_nelson_ifta_7d','vontoerne_nelson_ifta_56d','gse7887','gse13672_ratproxtub','gse13672_ratmtal',
+					  'gse6466_neonatal','gse6466_adult','gse22561','gse17142','hills_brunskill_proxtubtgfmrna');
+?>
